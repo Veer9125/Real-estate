d608705af38de7738e1c9292704886b0b7bb7d2d
Veer9125 <yuvraj9125sing@gmail.com>
2024-12-14 13:42:50 +0530

    handle submit functiionlity done

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-router-dom';
 function SignUp() {
+  
+  const [formData, setFormData] = useState({});
+  const handleChange = (e)=>{
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    })
+  };
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    const res = await fetch('/api/auth/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+    console.log(data);
+  }
+  console.log(formData);
+  
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form className='flex flex-col gap-4 focus:outline-none'>
-        <input type="text" placeholder='username' className='border p-3 rounded-lg focus:outline-none' id='username' />
-        <input type="text" placeholder='email' className='border p-3 rounded-lg focus:outline-none' id='email' />
-        <input type="text" placeholder='password' className='border p-3 rounded-lg focus:outline-none' id='password' />
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4 focus:outline-none'>
+        <input type="text" placeholder='username' className='border p-3 rounded-lg focus:outline-none' id='username' onChange={handleChange} />
+        <input type="email" placeholder='email' className='border p-3 rounded-lg focus:outline-none' id='email' onChange={handleChange} />
+        <input type="password" placeholder='password' className='border p-3 rounded-lg focus:outline-none' id='password' onChange={handleChange} />
         <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>SignUp</button>
       </form>
       <div className='flex gap-2 mt-5'>

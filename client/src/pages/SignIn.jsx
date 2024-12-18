77a040c55a5a3749f4dbf15a3476bd3276842d45
Veer9125 <yuvraj9125sing@gmail.com>
2024-12-18 12:55:35 +0530

    complete profile page ui

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -47,7 +47,7 @@ function SignIn() {
       <form onSubmit={handleSubmit} className='flex flex-col gap-4 focus:outline-none'>
         <input type="email" placeholder='email' className='border p-3 rounded-lg focus:outline-none' id='email' onChange={handleChange} />
         <input type="password" placeholder='password' className='border p-3 rounded-lg focus:outline-none' id='password' onChange={handleChange} />
-        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...' : 'Sign In'}</button>
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 duration-500 disabled:opacity-80'>{loading ? 'Loading...' : 'Sign In'}</button>
         <OAuth/>
       </form>
       <div className='flex gap-2 mt-5'>
